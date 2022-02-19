d006d6e55d660457eec911b1ff9eda733d3d5e27
iarawatson2 <iarawatson2>
2022-02-18 22:57:54 -0300

    simulando tela de login faltando storybook e adicionar na page e rota correta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,13 @@
+import { LoginComponent } from './pages/login/login.page';
+import { DashboardComponent } from './pages/dashboard/dashboard.page';
+import { ContasReceberComponent } from './pages/contas-receber/contas-receber.page';
+import { ClienteComponent } from './pages/cliente/cliente.page';
+import { TabelaComponent } from './components/tabela/tabela.component';
+import { PaginacaoComponent } from './components/paginacao/paginacao.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { InputComponent } from './components/input/input.component';
+import { IconComponent } from './components/icon/icon.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -6,9 +16,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    CheckboxComponent,
+    IconComponent,
+    InputComponent,
+    LogoComponent,
+    PaginacaoComponent,
+    TabelaComponent,
+    ClienteComponent,
+    ContasReceberComponent,
+    DashboardComponent,
+    LoginComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,12 +1,22 @@
+import { IconComponent } from './../../components/icon/icon.component';
+import { LogoComponent } from './../../components/logo/logo.component';
+import { CheckboxComponent } from './../../components/checkbox/checkbox.component';
+import { ButtonComponent } from './../../components/button/button.component';
+import { InputComponent } from './../../components/input/input.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    InputComponent,
+    ButtonComponent,
+    CheckboxComponent,
+    LogoComponent,
+    IconComponent
+  ],
+  imports: [CommonModule],
+  exports: []
 })
 export class LoginModule { }
