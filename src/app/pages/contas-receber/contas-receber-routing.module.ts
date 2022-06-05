d882aff24eee964bd29ce1f23be51c244062b125
iara <iara.watson5@hotmail.com>
2022-06-05 19:30:15 -0300

    Formulario conta receber

--- src/app/pages/contas-receber/contas-receber-routing.module.ts
+++ src/app/pages/contas-receber/contas-receber-routing.module.ts
@@ -3,15 +3,24 @@ import { ContasReceberComponent } from './contas-receber.page';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LiquidarContaComponent } from './liquidar-conta/liquidar-conta.component';
+import { ContaReceberComponent } from './contas-receber/conta-receber.component';
+import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: ContasReceberComponent
+    component: ContasReceberComponent,
+    canActivate: [AuthGuard]
   },
   {
     path     : ':new',
-    component: LiquidarContaComponent
+    component: LiquidarContaComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path     : ':id/:conta-receber',
+    component: ContaReceberComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
