3e8928f40d5451dce7b2abab224dea67da2b42c8
Danillo Leão Deucher <danilloleaolopes10@gmail.com>
2022-06-28 21:28:00 -0300

    correcao model

--- src/app/pages/contas-receber/filtro/model/filtro.contas.model.ts
+++ src/app/pages/contas-receber/filtro/model/filtro.contas.model.ts
@@ -13,7 +13,7 @@ export class IFiltroContas{
       this.descricao = data.descricao || '';
       this.vencimento_inicio = data.vencimento_inicio || '';
       this.vencimento_final = data.vencimento_final || '';
-      this.estado = data.estado || '';
+      this.estado = data.estado || 'MA';
       this.cidade = data.cidade || '';
       this.bairro = data.bairro || '';
       this.endereco = data.endereco || '';
